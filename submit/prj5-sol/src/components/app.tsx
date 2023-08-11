79343b95eb8d382d75d00ffd71d6708819d7a067
Hrudayangam-Mehta <124292975+Hrudayangam-Mehta@users.noreply.github.com>
2023-08-11 00:17:32 -0400

    Update app.tsx

--- submit/prj5-sol/src/components/app.tsx
+++ submit/prj5-sol/src/components/app.tsx
@@ -1,56 +1,60 @@
 import React, { useState } from 'react';
-import { Errors, makeElement } from '../lib/utils.js'; // Update the import path if needed
-import SpreadsheetWs from '../lib/ss-ws'; // Update the import path if needed
-import makeSpreadsheet from '../lib/spreadsheet.js'; // Update the import path if needed
+import ReactDOM from 'react-dom';
+import Spreadsheet from './spreadsheet';
+import SpreadsheetWs from '../lib/ss-ws';
+import { makeElement } from '../lib/utils';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Spreadsheet from './spreadsheet';
+// import SpreadsheetWs from '../lib/ss-ws';
 
+const WS_URL = 'https://zdu.binghamton.edu:2345'; // Update with the correct URL
 
-export default function App() {
-  const [wsUrl, setWsUrl] = useState('');
-  const [ssName, setSsName] = useState('');
-  const [errorMessages, setErrorMessages] = useState<string[]>([]);
-  
-  async function handleFormSubmit(event: React.FormEvent) {
+export const App: React.FC = () => {
+  const [ws, setWs] = useState<SpreadsheetWs | null>(null); // Use state to store ws
+  const [ssName, setSsName] = useState<string>(''); // Use state to store ssName
+
+  // Define a function to handle form submission
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setErrorMessages([]);
-
-    if (wsUrl.trim().length === 0 || ssName.trim().length === 0) {
-      setErrorMessages(['Both the Web Services Url and Spreadsheet Name must be specified']);
+    const wsUrlInput = document.querySelector('#ws-url') as HTMLInputElement;
+    const ssNameInput = document.querySelector('#ss-name') as HTMLInputElement;
+    const wsUrl = wsUrlInput.value.trim();
+    const ssName = ssNameInput.value.trim();
+    if (wsUrl.length === 0 || ssName.length === 0) {
+      alert('Both the Web Services Url and Spreadsheet Name must be specified');
     } else {
-      const ws = SpreadsheetWs.make(wsUrl);
-      await makeSpreadsheet(ws, ssName);
+      setWs(SpreadsheetWs.make(wsUrl)); // Set ws state
+      setSsName(ssName); // Set ssName state
     }
-  }
+  };
 
-  return (
-    <div id="app">
-      <form className="form" id="ss-form" onSubmit={handleFormSubmit}>
+  // Define a function to render the form
+  const renderForm = () => {
+    return (
+      <form className="form" id="ss-form" onSubmit={handleSubmit}>
         <label htmlFor="ws-url">Web Services Url</label>
-        <input
-          name="ws-url"
-          id="ws-url"
-          value={wsUrl}
-          onChange={(event) => setWsUrl(event.target.value)}
-        />
-
+        <input name="ws-url" id="ws-url" defaultValue={WS_URL} />
         <label htmlFor="ss-name">Spreadsheet Name</label>
-        <input
-          name="ss-name"
-          id="ss-name"
-          value={ssName}
-          onChange={(event) => setSsName(event.target.value)}
-        />
-
+        <input name="ss-name" id="ss-name" />
         <label></label>
         <button type="submit">Load Spreadsheet</button>
       </form>
+    );
+  };
 
-      <ul className="error" id="errors">
-        {errorMessages.map((message, index) => (
-          <li key={index}>{message}</li>
-        ))}
-      </ul>
+  // Define a function to render the spreadsheet
+  const renderSpreadsheet = () => {
+    return <Spreadsheet ws={ws!} ssName={ssName} />;
+  };
 
-      <div id="ss"></div>
+  return (
+    <div id="app">
+      {ws ? renderSpreadsheet() : renderForm()} 
     </div>
   );
-}
+};
+
+const rootElement = document.getElementById('app'); // Change 'root' to 'app'
+// ReactDOM.createRoot(rootElement).render(<App />); // Use createRoot instead of render
+ReactDOM.render(<App />, rootElement); // Use createRoot instead of render
